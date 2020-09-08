97316c28b5219393141ee13b4bd2ad083097872f
zhangchuangch <chuangchuang.zhang@waoqi.com>
2020-09-08 17:59:10 +0800

    Initial Commit

---
+++ ZCTZLmagePickerManager/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  ZCTZLmagePickerManager
+//
+//  Created by 张闯闯 on 2020/9/8.
+//  Copyright © 2020 张闯闯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ ZCTZLmagePickerManager/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ZCTZLmagePickerManager
+//
+//  Created by 张闯闯 on 2020/9/8.
+//  Copyright © 2020 张闯闯. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
